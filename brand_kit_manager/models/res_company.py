80cbb9c78ebd6dd580810ad360d4c82d1b16b26e
Abhisheksandeepa <89740367+Abhishekbandaranayaka@users.noreply.github.com>
2026-02-28 21:20:37 +0530

    Settings UI

--- brand_kit_manager/models/res_company.py
+++ brand_kit_manager/models/res_company.py
@@ -4,6 +4,25 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
+    brand_primary_color = fields.Char(
+        string="Brand Primary Color",
+        default="#0B5FFF",
+        help="Primary brand color used in outgoing emails.",
+    )
+    brand_secondary_color = fields.Char(
+        string="Brand Secondary Color",
+        default="#F4F6FA",
+        help="Secondary brand color used in outgoing emails.",
+    )
+    brand_footer_text = fields.Html(
+        string="Brand Footer Text",
+        help="Footer content shown in branded emails.",
+    )
+    brand_legal_text = fields.Html(
+        string="Brand Legal Text",
+        help="Legal/disclaimer content shown in branded emails.",
+    )
+
     brand_kit_id = fields.Many2one(
         "brand.kit",
         string="Default Brand Kit",
@@ -14,9 +33,29 @@ class ResCompany(models.Model):
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
+    brand_primary_color = fields.Char(
+        related="company_id.brand_primary_color",
+        readonly=False,
+        string="Brand Primary Color",
+    )
+    brand_secondary_color = fields.Char(
+        related="company_id.brand_secondary_color",
+        readonly=False,
+        string="Brand Secondary Color",
+    )
+    brand_footer_text = fields.Html(
+        related="company_id.brand_footer_text",
+        readonly=False,
+        string="Brand Footer Text",
+    )
+    brand_legal_text = fields.Html(
+        related="company_id.brand_legal_text",
+        readonly=False,
+        string="Brand Legal Text",
+    )
+
     brand_kit_id = fields.Many2one(
         related="company_id.brand_kit_id",
         readonly=False,
         string="Default Brand Kit",
     )
-
